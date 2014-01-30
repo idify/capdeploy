901241919b63e5e9e385d76fa069e3fc894ee20f
Gagan <developer.gagan@gmail.com>
2014-01-30 13:57:43 +0530

    deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capdeploy::Application.config.secret_key_base = 'd6a6297dda0c9f9f8a755062f7ef4ef950130519413e2c6044159e5add1fc303fe42123ab9124fbf44f6e5025f615a99aeeef23d2d04c4c9cb302adc6db0137b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capdeploy::Application.config.session_store :cookie_store, key: '_capdeploy_session'
